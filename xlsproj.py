038676ed5fcaf32fa2c913bb057c21f31a438812
catnparrot <idiotpi3rrot@gmail.com>
2024-03-18 20:10:17 +0900

    데이터추출-저장 성공

--- xlsproj.py
+++ xlsproj.py
@@ -3,7 +3,7 @@ import pathlib
 import pandas as pd
 
 # 연도 설정
-YEAR = "2023"
+YEAR = "2022"
 
 # 점검대상
 target = "pathName"
@@ -16,21 +16,11 @@ output_data = pd.DataFrame()
 
 # 파일 경로 설정
 folder_base_path_string = f'C:\\Users\\man\\dir\\{target}\\folder'
-#folder_base_path_obj = pathlib.Path(folder_base_path_string)
-#folder_base_path = os.path.dirname(folder_base_path_obj)
 
 # 1월~12월 데이터 설정
 for MONTH in range(1, 13):
     # 파일 경로 설정
-    #folder_path_string = f'{folder_base_path_string}\\{YEAR}\\{target} {YEAR}년 {MONTH}월 문서'
     folder_path = f'{folder_base_path_string}\\{YEAR}\\{target} {YEAR}년 {MONTH}월 문서'
-    #folder_path_obj = pathlib.Path(folder_path_string)
-    #folder_path = os.path.join(folder_base_path, folder_path_obj)
-    #folder_path = os.path.dirname(folder_path_obj)
-    
-    #print(f'folder_path: {folder_path}')
-    #print(f'folder_path_obj: {folder_path_obj}')
-    #print(f'folder_path_string: {folder_path_string}')
 
     if os.path.exists(folder_path) and os.path.isdir(folder_path):  # 디렉토리가 존재하는지 확인
         # 디렉토리 내의 파일을 검색
@@ -41,7 +31,7 @@ for MONTH in range(1, 13):
                 print(f'filepath: {filepath}')
 
                 # 엑셀 파일에서 모든 시트를 읽어옴
-                excel_data = pd.read_excel(filepath, sheet_name=None, header=None)
+                excel_data = pd.read_excel(filepath, sheet_name=None, header=None, usecols="E", skiprows=11, nrows=1)
                 
                 print(f'excel_data: {excel_data}')
 
@@ -51,16 +41,15 @@ for MONTH in range(1, 13):
 
                         print(f'test-location: {location}')
 
-                        row, col = pd.ExcelWriter.parse(location)
                         try:
-                            value = sheet_data.iloc[row - 1, col - 1]
+                            value = sheet_data.iloc[0, 0]
                         except IndexError:
                             value = None
 
                         # 추출한 데이터를 DataFrame에 추가
-                        output_data = output_data.append(
-                            {'File': filename, 'Sheet': sheet_name, 'Cell': location, 'Value': value},
-                            ignore_index=True)
+                        df=pd.DataFrame( {'File': filename, 'Sheet': sheet_name, 'Cell': location, 'Value': value},
+                            index=[1, 2])
+                        output_data = output_data._append(df)
 
 # 추출된 데이터를 하나의 엑셀 파일에 저장
 output_file_string = f'C:\\Users\\man\\dir\\{target}\\data.xlsx'  # 결과 파일 경로로 수정
